77fbe8a6eeaed3147c912fd88d85beecf5791f6f
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2016-06-17 10:50:22 +1000

    Direct logging module to use console for logging.

--- hello_world/settings.py
+++ hello_world/settings.py
@@ -122,3 +122,19 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'INFO'),
+        },
+    },
+}
